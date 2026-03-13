3f3c09c6afd71f48c46f7ab54426bc8fdb091207
Jan Říha <rihaj@vse.cz>
2026-03-13 14:40:03 +0100

    Vytvoření spouštěcích tříd s loggery.

---
+++ projects/sockets/client/src/main/java/client/Client.java
@@ -0,0 +1,14 @@
+package client;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Client
+{
+    private static final Logger log = LoggerFactory.getLogger(Client.class);
+
+    public static void main(String[] args)
+    {
+        log.info("Client started.");
+    }
+}
---
+++ projects/sockets/server/src/main/java/server/Server.java
@@ -0,0 +1,14 @@
+package server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Server
+{
+    private static final Logger log = LoggerFactory.getLogger(Server.class);
+
+    public static void main(String[] args)
+    {
+        log.info("Server started.");
+    }
+}
