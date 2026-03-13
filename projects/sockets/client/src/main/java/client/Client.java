aa637dc9b260028e3c7890b3824e456574d4ec81
Jan Říha <rihaj@vse.cz>
2026-03-13 15:03:14 +0100

    Základní navázání síťového spojení.
    Server po spuštění čeká na příchozí spojení na portu 8080, klient se po spuštění pokusí k serveru připojit. Jakmile je spojení úspěšně navázáno, obě aplikace se ukončí (na pozadí dojde pouze k základnímu TCP handshaku, aplikace si ale neposílají žádná uživatelská data).

--- projects/sockets/client/src/main/java/client/Client.java
+++ projects/sockets/client/src/main/java/client/Client.java
@@ -3,6 +3,10 @@ package client;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Client
 {
     private static final Logger log = LoggerFactory.getLogger(Client.class);
@@ -10,5 +14,14 @@ public class Client
     public static void main(String[] args)
     {
         log.info("Client started.");
+
+        try {
+            // Vytvoří socket a pokusí se navázat síťové spojení se serverem
+            Socket socket = new Socket("127.0.0.1", 8080);
+        } catch (UnknownHostException e) {
+            log.error("Error occurred while connecting to server.", e);
+        } catch (IOException e) {
+            log.error("Error occurred in network communication.", e);
+        }
     }
 }
--- projects/sockets/server/src/main/java/server/Server.java
+++ projects/sockets/server/src/main/java/server/Server.java
@@ -3,6 +3,9 @@ package server;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class Server
 {
     private static final Logger log = LoggerFactory.getLogger(Server.class);
@@ -10,5 +13,16 @@ public class Server
     public static void main(String[] args)
     {
         log.info("Server started.");
+
+        try {
+            // Vytvoří server-socket pro příjem požadavků o síťové spojení
+            ServerSocket serverSocket = new ServerSocket(8080);
+
+            // Aktivně čeká na příchozí požadavek o spojení (aplikace na tomto
+            // řádku čeká, dokud spojení není navázáno)
+            serverSocket.accept();
+        } catch (IOException e) {
+            log.error("Error occurred in network communication.", e);
+        }
     }
 }
