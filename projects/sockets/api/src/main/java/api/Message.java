aa5b79179ad2b15fdf77439bca730b951faee139
Jan Říha <rihaj@vse.cz>
2026-03-13 18:42:42 +0100

    Serializovatelná třída Message.
    Objekty této třídy představují zprávy zasíláné mezi klientem a serverem. V této podobě se jedná o poměrně naivní implementaci, která byla vytvořena tak, aby šla co nejjednodušeji začlenit do stávajícího projektu. Při implementaci skutečné aplikace by bylo vhodnější typ zprávy (první slovo zasílané v atributu body) vyčlenit do samostatného atributu řešeného pomocí enumerace, případně pro každý typ zprávy vytvořit samostatnou třídu, ideálně potomka třídy Message.

--- projects/sockets/api/src/main/java/api/Message.java
+++ projects/sockets/api/src/main/java/api/Message.java
@@ -1,6 +1,27 @@
 package api;
 
-public class Message
-{
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
 
+public class Message implements Serializable
+{
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private final LocalDateTime timestamp;
+    private final String body;
+
+    public Message(String body) {
+        this.timestamp = LocalDateTime.now();
+        this.body = body;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public String getBody() {
+        return body;
+    }
 }
