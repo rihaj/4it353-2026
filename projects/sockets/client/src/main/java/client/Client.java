d229e6b86e47e05ddc3a3e54733d6fb55a35e958
Jan Říha <me@rihaj.cz>
2026-03-23 17:50:27 +0100

    Reálná komunikace na straně klienta.
    Klient běží ve dvou vláknech. První načítá vstup z konzole a odesílá ho na server. Druhé přijímá data ze serveru a vypisuje je na konzoli.

--- projects/sockets/client/src/main/java/client/Client.java
+++ projects/sockets/client/src/main/java/client/Client.java
@@ -15,5 +15,32 @@ public class Client
     public static void main(String[] args)
     {
         log.info("Client started.");
+
+        try (Scanner scanner = new Scanner(System.in);
+             Socket socket = new Socket("127.0.0.1", 8080)) {
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
+            PrintWriter pw = new PrintWriter(socket.getOutputStream(), true, StandardCharsets.UTF_8);
+
+            Thread listenerThread = new Thread(() -> {
+                try {
+                    while (true) {
+                        System.out.println(br.readLine());
+                    }
+                } catch (IOException e) {
+                    log.error("Error occurred in network communication.", e);
+                }
+            }, "Listener");
+
+            listenerThread.start();
+
+            while (true) {
+                pw.println(scanner.nextLine());
+            }
+        } catch (IOException e) {
+            log.error("Error occurred in network communication.", e);
+        }
+
+        log.info("Client terminated.");
     }
 }
